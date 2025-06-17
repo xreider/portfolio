8132f73f1a6833b5848fb079735621c6010acaf8
xreider <xreider@yandex.ru>
2025-06-17 23:26:05 +1000

    first commit

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -42,35 +42,35 @@ export const LandingPage = () => {
         <p className={'muted'}>
           Собирал сайты ещё <em>с 2006 года</em> как хобби. <em>С 2019 по 2023</em> года активно изучал <em>основные цифровые технологии</em> (фуллстек программирование react, typescript, вёрстка, nodejs, mongodb, docker и другие технологии, 3Д моделирование и 3Д программирование, цифровые редакторы, вектор, ретушь, фотобаш, архитектурный дизайн). <em>В 2023 году</em>, имея знания вёрстки и фронтенда, столкнулся с трудностями организации интерфейса проекта для портфолио программиста, и именно в этот момент стал усиленно проходить курсы UX UI, и <em>задачи разработки и тестирования интерфейсов</em> увлекли меня больше всех. <em>В октябре 2023</em> года успешно прошёл собеседование в 3 компании как программист и в 1 компанию как UX UI дизайнер, но <em>устроился именно дизайнером в ООО {isHiddenMode ? '«Школа дизайна»' : "«4А»"}</em>.
         </p>
-      </div>
-      <div className={cls.section}>
-        <BioItem emoji={<Icon name='telegram' />} text={<>
-          Телеграмм <ExternalLink href='https://t.me/Rumar1' accent='default'>@Rumar1</ExternalLink>
-        </>} />
-        <BioItem emoji={<Icon name='vk' />} text={<>
-          ВКонтакте <ExternalLink href='https://vk.com/indigosay' accent='default'>@indigosay</ExternalLink>
-        </>} />
-        <BioItem emoji={'✉️'} text={<>
-          Емайл <ExternalLink href='mailto:xreider@yandex.ru' accent='default'>xreider@yandex.ru</ExternalLink>
-        </>} />
-        <BioItem emoji={'⏰'} text={<>
-          Желаемое время работы до 14-18 МСК
-        </>} />
-        <BioItem emoji={'🌇'} text={<>
-          Город Хабаровск, Россия (+10 МСК)
-        </>} />
-        <BioItem emoji={'🎂'} text={<>
-          Возраст {myAge}
-        </>} />
-        <BioItem emoji={'👨‍👩‍👧'} text={<>
-          Семейное положение: женат, ребёнок
-        </>} />
-        <BioItem emoji={'🀄'} text={<>
-          Образование: высшее, переводчик-лингвист китайского и английского языков
-        </>} />
-        <BioItem emoji={'⚔️'} text={<>
-          Срочная служба в армии
-        </>} />
+        <div className={cls.subsection}>
+          <BioItem emoji={<Icon name='telegram' />} text={<>
+            Телеграмм <ExternalLink href='https://t.me/Rumar1' accent='default'>@Rumar1</ExternalLink>
+          </>} />
+          <BioItem emoji={<Icon name='vk' />} text={<>
+            ВКонтакте <ExternalLink href='https://vk.com/indigosay' accent='default'>@indigosay</ExternalLink>
+          </>} />
+          <BioItem emoji={'✉️'} text={<>
+            Емайл <ExternalLink href='mailto:xreider@yandex.ru' accent='default'>xreider@yandex.ru</ExternalLink>
+          </>} />
+          <BioItem emoji={'⏰'} text={<>
+            Желаемое время работы до 14-18 МСК
+          </>} />
+          <BioItem emoji={'🌇'} text={<>
+            Город Хабаровск, Россия (+10 МСК)
+          </>} />
+          <BioItem emoji={'🎂'} text={<>
+            Возраст {myAge}
+          </>} />
+          <BioItem emoji={'👨‍👩‍👧'} text={<>
+            Семейное положение: женат, ребёнок
+          </>} />
+          <BioItem emoji={'🀄'} text={<>
+            Образование: высшее, переводчик-лингвист китайского и английского языков
+          </>} />
+          <BioItem emoji={'⚔️'} text={<>
+            Срочная служба в армии
+          </>} />
+        </div >
       </div >
       <div className={cls.section}>
         <h3>Опыт работы</h3>
@@ -96,36 +96,89 @@ export const LandingPage = () => {
         <h3>Программы</h3>
         <div className={cls.subsection}>
           <h4>Профессиональные инструменты</h4>
+          <p><ExternalLink href='https://www.figma.com/' accent='high'>Figma</ExternalLink>,{' '}
+            <ExternalLink href='https://webflow.com/' accent='high'>Webflow🪫</ExternalLink>,{' '}
+            <ExternalLink href='https://www.framer.com/' accent='high'>Framer🪫</ExternalLink>
+          </p>
         </div>
         <div className={cls.subsection}>
           <h4>Графические редакторы</h4>
+          <p><ExternalLink href='https://www.adobe.com/products/illustrator.html' accent='high'>Illustrator</ExternalLink> (проходил несколько курсов, делал брендинговые логотипы, иконки), <ExternalLink href='https://www.adobe.com/products/photoshop.html' accent='high'>Photoshop</ExternalLink> (проходил курсы по всем функциям, ретуши, цветокору, фотобашу, matte-painting), <ExternalLink href='https://www.adobe.com/products/aftereffects.html' accent='high'>After Effects🪫</ExternalLink> (проходил курсы и обучение по коротким видео для разных задач, делал различные простые анимированные элементы для лендингов, делал позиционирование лого на движущемся видео)
+          </p>
+        </div>
+        <div className={cls.subsection}>
+          <h4>3Д</h4>
+          <p className={'muted'}>Проходил множество курсов по 3Д, но на этапе изучения софт-сёрфис понял, что мне достаточно знать хард-сёрфис
+          </p>
+          <p>
+            <ExternalLink href='https://www.blender.org/' accent='high'>Blender</ExternalLink>,{' '}
+            <ExternalLink href='https://www.adobe.com/products/substance3d/apps/painter.html' accent='high'>Adobe Substance 3D🪫</ExternalLink>
+          </p>
         </div>
         <div className={cls.subsection}>
           <h4>Планирование</h4>
+          <p><ExternalLink href='https://clickup.com/' accent='high'>ClickUp</ExternalLink>,{' '}
+            <ExternalLink href='https://miro.com/' accent='high'>Miro</ExternalLink>,{' '}
+            <ExternalLink href='https://app.diagrams.net/' accent='high'>Draw.io</ExternalLink>,{' '}
+            <ExternalLink href='https://app.clockify.me/' accent='high'>Clockify</ExternalLink>,{' '}
+            <ExternalLink href='https://www.focustodo.cn/' accent='high'>Focus To-Do</ExternalLink> (методика Помодоро)
+          </p>
         </div>
       </div>
       <div className={cls.section}>
         <h3>Искуственный Интеллект</h3>
         <div className={cls.subsection}>
           <h4>Генерация приложений и дизайна интерфейсов</h4>
-          <p className={'muted'}>
-            Использую для тестирования гипотез
+          <p className={'muted'}>Использую для тестирования гипотез и поиска вдохновений
           </p>
           <p>
-            <ExternalLink href='https://v0.dev/' accent='high'>v0.dev</ExternalLink>, <ExternalLink href='https://lovable.dev/' accent='high'>lovable.dev</ExternalLink>, <ExternalLink href='https://studio.firebase.google.com/' accent='high'>studio.firebase.google.com</ExternalLink> (понимает только английский), <ExternalLink href='https://bolt.new/' accent='high'>bolt.new</ExternalLink>, <ExternalLink href='https://adaptive.ai/' accent='high'>adaptive.ai</ExternalLink>, <ExternalLink href='https://replit.com/' accent='high'>replit.com</ExternalLink>  (понимает только английский), <ExternalLink href='https://gamma.app/' accent='high'>gamma.app</ExternalLink>, <ExternalLink href='https://stitch.withgoogle.com/' accent='high'>stitch.withgoogle.com</ExternalLink>
+            <ExternalLink href='https://v0.dev/' accent='high'>v0.dev</ExternalLink>,{' '}
+            <ExternalLink href='https://lovable.dev/' accent='high'>lovable.dev</ExternalLink>,{' '}
+            <ExternalLink href='https://studio.firebase.google.com/' accent='high'>studio.firebase.google.com</ExternalLink> (понимает только английский), <ExternalLink href='https://bolt.new/' accent='high'>bolt.new</ExternalLink>,{' '}
+            <ExternalLink href='https://adaptive.ai/' accent='high'>adaptive.ai</ExternalLink>,{' '}
+            <ExternalLink href='https://replit.com/' accent='high'>replit.com</ExternalLink> (понимает только английский),{' '}
+            <ExternalLink href='https://gamma.app/' accent='high'>gamma.app</ExternalLink>,{' '}
+            <ExternalLink href='https://stitch.withgoogle.com/' accent='high'>stitch.withgoogle.com</ExternalLink>
           </p>
         </div>
         <div className={cls.subsection}>
           <h4>Изображения</h4>
+          <p><ExternalLink href='https://github.com/AUTOMATIC1111/stable-diffusion-webui' accent='high'>Stable diffusion</ExternalLink>,{' '}
+            <ExternalLink href='https://github.com/lllyasviel/Fooocus' accent='high'>Fooocus</ExternalLink>,{' '}
+            <ExternalLink href='https://chatgpt.com/' accent='high'>Chatgpt</ExternalLink>,{' '}
+            <ExternalLink href='https://www.perplexity.ai/' accent='high'>Perplexity</ExternalLink>,{' '}
+            <ExternalLink href='https://www.midjourney.com/' accent='high'>Midjourney</ExternalLink>
+          </p>
         </div>
         <div className={cls.subsection}>
           <h4>Видео</h4>
+          <p className={'muted'}>Использовал для оживления сторисов в прототипе заказчика
+          </p>
+          <p><ExternalLink href='https://runwayml.com/' accent='high'>Runway</ExternalLink>,{' '}
+            <ExternalLink href='https://veo3.ai/' accent='high'>Veo3</ExternalLink>
+          </p>
         </div>
         <div className={cls.subsection}>
           <h4>3Д</h4>
+          <p className={'muted'}>Использовал для создания 3Д модели логотипа компании, но в итоге сделал всё руками в Блендере
+          </p>
+          <p><ExternalLink href='https://www.meshy.ai/' accent='high'>meshy.ai</ExternalLink>,{' '}
+            <ExternalLink href='https://www.tripo3d.ai/' accent='high'>tripo3d.ai</ExternalLink>,{' '}
+            <ExternalLink href='https://hunyuan3d.ai/' accent='high'>hunyuan3d.ai</ExternalLink>
+          </p>
         </div>
         <div className={cls.subsection}>
           <h4>Текстовые запросы</h4>
+          <p><ExternalLink href='https://gemini.google.com/' accent='high'>Gemini</ExternalLink>,{' '}
+            <ExternalLink href='https://grok.com/' accent='high'>Grok</ExternalLink>,{' '}
+            <ExternalLink href='https://chatgpt.com/' accent='high'>Chatgpt</ExternalLink>,{' '}
+            <ExternalLink href='https://www.anthropic.com/' accent='high'>Claude</ExternalLink>,{' '}
+            <ExternalLink href='https://www.perplexity.ai/' accent='high'>Perplexity</ExternalLink>,{' '}
+            <ExternalLink href='https://chat.qwen.ai/' accent='high'>Qwen</ExternalLink>,{' '}
+            <ExternalLink href='https://chat.deepseek.com/' accent='high'>Deepseek</ExternalLink>,{' '}
+            <ExternalLink href='https://copilot.microsoft.com/' accent='high'>Copilot</ExternalLink>,{' '}
+            <ExternalLink href='https://windsurf.com/' accent='high'>Windsurf</ExternalLink>
+          </p>
         </div>
       </div>
       <div className={cls.section}>
@@ -145,6 +198,28 @@ export const LandingPage = () => {
         </div>
         <div className={cls.subsection}>
           <h4>Список изучаемых медиа</h4>
+        </div>
+        <div className={cls.subsection}>
+          <h4>Список изучаемых веб-технологий</h4>
+        </div>
+      </div>
+
+
+      <div className={cls.section}>
+        <h2>Поговорим о Ваших проектах!</h2>
+        <p className={'body-lg'}>
+          Ищу команду мечты с душевными людьми и отработанными процессами исследований и проектировки интерфейсов
+        </p>
+        <div className={cls.subsection}>
+          <BioItem emoji={<Icon name='telegram' />} text={<>
+            Телеграмм <ExternalLink href='https://t.me/Rumar1' accent='default'>@Rumar1</ExternalLink>
+          </>} />
+          <BioItem emoji={<Icon name='vk' />} text={<>
+            ВКонтакте <ExternalLink href='https://vk.com/indigosay' accent='default'>@indigosay</ExternalLink>
+          </>} />
+          <BioItem emoji={'✉️'} text={<>
+            Емайл <ExternalLink href='mailto:xreider@yandex.ru' accent='default'>xreider@yandex.ru</ExternalLink>
+          </>} />
         </div>
       </div>
     </>
