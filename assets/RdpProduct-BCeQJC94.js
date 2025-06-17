b48bf81f570d5beae57e501811469681cdb91828
xreider <xreider@yandex.ru>
2025-06-17 11:14:55 +1000

    Updates

---
+++ assets/RdpProduct-BCeQJC94.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CUuA4P0D.js";const p="src-pages-product-rdp-RdpProduct-module_wrapper__6x2l-",e={wrapper:p},t=()=>r.jsx("div",{className:e.wrapper,children:"Rdp"});export{t as RdpProduct};
--- assets/RdpProduct-Czm94YoK.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DW9SufQe.js";const p="src-pages-product-rdp-RdpProduct-module_wrapper__6x2l-",e={wrapper:p},t=()=>r.jsx("div",{className:e.wrapper,children:"Rdp"});export{t as RdpProduct};
